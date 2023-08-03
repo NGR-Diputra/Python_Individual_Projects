257ad05aa5218310ae42c0f04f9beadc82f8bf2a
NGRH-Diputra <120901630+NGR-Diputra@users.noreply.github.com>
2023-08-03 10:10:50 +0800

    Upload Main File for MP3 Music Player

---
+++ project1.py
@@ -0,0 +1,12 @@
+from pygame import mixer
+from tkinter import *
+import tkinter.font as font
+from tkinter import filedialog
+
+#Root Window
+root = Tk()
+root.title(" DataFlair Python MP3 Music Player App ")
+
+#mixer
+mixer.init()
+
